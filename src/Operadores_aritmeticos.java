58a66380bc6c55d4221121578f29a36fbdfc3d97
josedevjava <joseantonio.munoz2397@gmail.com>
2026-07-19 19:39:23 -0400

    Java basico

---
+++ src/Operadores_aritmeticos.java
@@ -0,0 +1,13 @@
+public class Operadores_aritmeticos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.println("El valor de a es: "+a+" "+"y el valor de b es: "+b);
+        System.out.println("La suma de a+b es igual a "+(a + b));
+        System.out.println("La resta de a-b es igual a "+(a - b));
+        System.out.println("La multiplicacion de a*b es igual a "+(a * b));
+        System.out.println("La division de a/b es igual a "+(a / b));
+        System.out.println("El modulo de a%b es igual a "+(a % b));
+
+    }
+}
