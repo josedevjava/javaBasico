d00de9d9fab0b634715d7cce3b1bb0a3dad9745b
josedevjava <joseantonio.munoz2397@gmail.com>
2026-07-19 21:56:57 -0400

    Adivina un numero

---
+++ src/Loops.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import javax.swing.UIManager;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName()
+            );
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,
+                    "A ocurrido un problema" + e.getMessage());
+        }
+
+        int numeroSecreto = 40;
+        int intento = 0;
+
+        while (intento != numeroSecreto) {
+
+            String texto = JOptionPane.showInputDialog(null,
+                    "Ingrese un numero del 1 al 100.");
+
+            if (texto == null) {
+
+                JOptionPane.showMessageDialog(null, "Canselaste el programa");
+                return;
+            }
+
+            intento = Integer.parseInt(texto);
+
+            if (intento > numeroSecreto){
+
+                JOptionPane.showMessageDialog(null,
+                        "El numero secreto es menor que el ingresado");
+
+            } else if (intento < numeroSecreto){
+
+                JOptionPane.showMessageDialog(null,
+                        "El numero secreto es mayor que el ingresado");
+
+            }
+
+        }
+
+        JOptionPane.showMessageDialog(null,
+                "Felicidades adivinaste el numero!");
+
+    }
+}
+
